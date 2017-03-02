b3caff9a8a683099ea642aaa2a4335efc66322be
Smith <ashley@goandy.net>
2017-03-02 09:51:26 +0000

    Blank slate - code commented so that only a black screen is rendered

--- Racer2/Game.cpp
+++ Racer2/Game.cpp
@@ -29,41 +29,41 @@ void Game::Load()
 	GetIAudioMgr()->Initialise();
 	mLoadData.loadedSoFar++;
 
-	Mesh& ecar = mMeshMgr.CreateMesh("ferrari");
-	ecar.CreateFrom("data/ferrari.x", gd3dDevice, mFX.mCache);
-	mCar.Initialise(ecar);
-	mCar.GetPosition() = Vector3(0, -2, 6);
-	mCar.GetMesh().GetSubMesh(1).material.gfxData.Set(Vector4(1,1,1,1), Vector4(1,1,1,1), Vector4(0.125f, 0.125f, 0.05f, 5));  //body has a touch of speculr shinyness
-	mCar.GetMesh().GetSubMesh(0).material.gfxData.Set(Vector4(1, 1, 1, 1), Vector4(1, 1, 1, 1), Vector4(0, 0, 0, 1));  //tyres are not shiny!
-	mLoadData.loadedSoFar++;
+	//Mesh& ecar = mMeshMgr.CreateMesh("ferrari");
+	//ecar.CreateFrom("data/ferrari.x", gd3dDevice, mFX.mCache);
+	//mCar.Initialise(ecar);
+	//mCar.GetPosition() = Vector3(0, -2, 6);
+	//mCar.GetMesh().GetSubMesh(1).material.gfxData.Set(Vector4(1,1,1,1), Vector4(1,1,1,1), Vector4(0.125f, 0.125f, 0.05f, 5));  //body has a touch of speculr shinyness
+	//mCar.GetMesh().GetSubMesh(0).material.gfxData.Set(Vector4(1, 1, 1, 1), Vector4(1, 1, 1, 1), Vector4(0, 0, 0, 1));  //tyres are not shiny!
+	//mLoadData.loadedSoFar++;
 }
 
 void Game::LoadDisplay(float dTime)
 {
 	BeginRender(Colours::Black);
 
-	mpSpriteBatch->Begin();
+	//mpSpriteBatch->Begin();
 
 	static int pips = 0;
 	static float elapsed = 0;
 	elapsed += dTime;
-	if (elapsed > 0.25f){
-		pips++;
-		elapsed = 0;
-	}
-	if (pips > 10)
-		pips = 0;
-	wstringstream ss;
-	ss << L"Loading meshes(" << (int)(((float)mLoadData.loadedSoFar / (float)mLoadData.totalToLoad)*100.f) << L"%) ";
-	for (int i = 0; i < pips; ++i)
-		ss << L'.';
-	mpFont2->DrawString(mpSpriteBatch, ss.str().c_str(), Vector2(100, 200), Colours::White, 0, Vector2(0, 0), Vector2(1.f, 1.f));
+	//if (elapsed > 0.25f){
+	//	pips++;
+	//	elapsed = 0;
+	//}
+	//if (pips > 10)
+	//	pips = 0;
+	//wstringstream ss;
+	//ss << L"Loading meshes(" << (int)(((float)mLoadData.loadedSoFar / (float)mLoadData.totalToLoad)*100.f) << L"%) ";
+	//for (int i = 0; i < pips; ++i)
+	//	ss << L'.';
+	//mpFont2->DrawString(mpSpriteBatch, ss.str().c_str(), Vector2(100, 200), Colours::White, 0, Vector2(0, 0), Vector2(1.f, 1.f));
 
-	ss.str(L"");
-	ss << L"FPS:" << (int)(1.f / dTime);
-	mpFont->DrawString(mpSpriteBatch, ss.str().c_str(), Vector2(10, 550), Colours::White, 0, Vector2(0, 0), Vector2(0.5f, 0.5f));
+	//ss.str(L"");
+	//ss << L"FPS:" << (int)(1.f / dTime);
+	//mpFont->DrawString(mpSpriteBatch, ss.str().c_str(), Vector2(10, 550), Colours::White, 0, Vector2(0, 0), Vector2(0.5f, 0.5f));
 
-	mpSpriteBatch->End();
+	//mpSpriteBatch->End();
 
 	EndRender();
 }
@@ -75,13 +75,13 @@ void Game::Initialise()
 
 	FX::SetupDirectionalLight(0, true, Vector3(-0.7f, -0.7f, 0.7f), Vector3(0.9f, 0.85f, 0.85f), Vector3(0.1f, 0.1f, 0.1f), Vector3(1, 1, 1));
 
-	mpSpriteBatch = new SpriteBatch(gd3dImmediateContext);
-	assert(mpSpriteBatch);
-	mpFont = new SpriteFont(gd3dDevice, L"data/comicSansMS.spritefont");
-	assert(mpFont);
+	//mpSpriteBatch = new SpriteBatch(gd3dImmediateContext);
+	//assert(mpSpriteBatch);
+	//mpFont = new SpriteFont(gd3dDevice, L"data/comicSansMS.spritefont");
+	//assert(mpFont);
 
-	mpFont2 = new SpriteFont(gd3dDevice, L"data/algerian.spritefont");
-	assert(mpFont2);
+	//mpFont2 = new SpriteFont(gd3dDevice, L"data/algerian.spritefont");
+	//assert(mpFont2);
 
 	mLoadData.totalToLoad = 2;
 	mLoadData.loadedSoFar = 0;
@@ -96,18 +96,18 @@ void Game::Release()
 {
 	mFX.Release();
 	mMeshMgr.Release();
-	delete mpFont;
+	/*delete mpFont;
 	mpFont = nullptr;
 	delete mpSpriteBatch;
 	mpSpriteBatch = nullptr;
 	delete mpFont2;
-	mpFont2 = nullptr;
+	mpFont2 = nullptr;*/
 }
 
 void Game::Update(float dTime)
 {
 	mGamepad.Update();
-	GetIAudioMgr()->Update();
+	/*GetIAudioMgr()->Update();
 
 	const float camInc = 10.f * dTime;
 
@@ -131,7 +131,7 @@ void Game::Update(float dTime)
 	mCamPos.z += mGamepad.GetState(0).leftStickY * dTime;
 	mCamPos.y += mGamepad.GetState(0).rightStickY * dTime;
 
-	mCar.GetRotation().y = GetClock();
+	mCar.GetRotation().y = GetClock();*/
 }
 
 
@@ -158,21 +158,21 @@ void Game::Render(float dTime)
 
 	CreateViewMatrix(FX::GetViewMatrix(), Vector3(0,0,-6), Vector3(0, 0, 0), Vector3(0, 1, 0));
 
-	mFX.Render(mCar, gd3dImmediateContext);
+	//mFX.Render(mCar, gd3dImmediateContext);
 
 
 	CommonStates state(gd3dDevice);
-	mpSpriteBatch->Begin(SpriteSortMode_Deferred, state.NonPremultiplied());
+	//mpSpriteBatch->Begin(SpriteSortMode_Deferred, state.NonPremultiplied());
 
-	//general messages
-	wstringstream ss;
-	if (dTime > 0)
-		ss << L"FPS: " << (int)(1.f / dTime);
-	else
-		ss << L"FPS: 0";
-	mpFont->DrawString(mpSpriteBatch, ss.str().c_str(), Vector2(10, 550), Colours::White, 0, Vector2(0, 0), Vector2(0.5f, 0.5f));
+	////general messages
+	//wstringstream ss;
+	//if (dTime > 0)
+	//	ss << L"FPS: " << (int)(1.f / dTime);
+	//else
+	//	ss << L"FPS: 0";
+	//mpFont->DrawString(mpSpriteBatch, ss.str().c_str(), Vector2(10, 550), Colours::White, 0, Vector2(0, 0), Vector2(0.5f, 0.5f));
 
-	mpSpriteBatch->End();
+	//mpSpriteBatch->End();
 
 
 	EndRender();
